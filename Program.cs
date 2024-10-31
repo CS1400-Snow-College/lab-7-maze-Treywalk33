f86a98447fbe219ca4d91dc2ab817f0af7e2ac7b
Trey Walker <Treywalk33@gmail.com>
2024-10-30 19:53:22 -0600

    step 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// Trey Walker, 10/30/24/ Maze
-Console.WriteLine("In this program there will be a maze put on screen with the goal to get to the end.");
+
+using System;
+
+namespace MazeGame
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In this program there will be a maze put on screen with the goal to get to the end.");
+
+        }
+    }
+}
+
 
